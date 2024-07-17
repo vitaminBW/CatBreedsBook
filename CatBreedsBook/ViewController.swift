6009e92b7e6c232d981e8364b6ccdee448fa4aa4
Vitaliy Bal <vitalik.bal@gmail.com>
2024-07-18 00:15:06 +0300

    Initial Commit

---
+++ CatBreedsBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CatBreedsBook
+//
+//  Created by Vitaliy Bal on 18.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
